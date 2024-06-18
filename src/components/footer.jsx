a39b8aed539f79d32d13d71605ef6a8c0a79d669
Akane <akane1033@gmail.com>
2024-06-18 14:48:15 +0200

    Move Footer component to "component" folder

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className='border-top text-secondary px-5 py-2'>
+      Build by <a target='_blank' href='https://akaneotake-portfolio.netlify.app'>Akane Otake</a>
+      <span className='float-end'>favicon by <a target='_blank' href='https://icons8.com'>Icons8</a></span>
+    </div>
+  );
+};
+
+export default Footer;
